c7f3cd5a92bfc3dce6b7bcbfdc0347709ee88a0f
Jayprecode <jayprecode@gmail.com>
2021-02-04 23:14:34 +0100

    Frontend & Api

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjmxTEbCfdjILVjjDE5Yu7mQ6JsRVhlyY",
+  authDomain: "netflix-clone-2-80bf2.firebaseapp.com",
+  projectId: "netflix-clone-2-80bf2",
+  storageBucket: "netflix-clone-2-80bf2.appspot.com",
+  messagingSenderId: "933648281648",
+  appId: "1:933648281648:web:535ef77cf8febb6db9bdf5",
+  measurementId: "G-MPSWZCX83R"
+};
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./LoginScreen.css";
+
+function LoginScreen() {
+	return (
+		<div className="loginScreen">
+			<div className="loginScreen__background">
+				<img
+					className="loginScreen__logo"
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+					alt=""
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default LoginScreen;
